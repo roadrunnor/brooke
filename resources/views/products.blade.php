5cd63adde85da9538d5cd529d18dacac1d573ca3
M V <mehdivakily2002@gmail.com>
2021-04-23 02:48:13 -0700

    product with photo in Sprint1

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,29 +2,21 @@
         <div class="container-fluid">
             <div class="row">
                 @foreach ($response as $product)
-                <div class="col-3">
-                    
-                    @csrf
+                    <div class="col-3">
+                        @csrf
                         <div class="jumbotron">
                             <a class="navbar-brand" href="{{ url('/details')}}">
-                              {{-- <img src="{{ asset('images/book7.jpg') }}" width="200px"> --}}
-                              <img src="{{ asset('images/'.$product->product_image) }}" width="200px">
-
+                                <img src="{{ asset('images/'.$product->product_image) }}" width="200px">
                             </a>
-                        <p> Title : {{$product->product_name}}</p>
-                        <p> Description : {{$product->product_description}}</p>
-                        <p> Price : {{$product->product_price}}</p>
-                       
-                        <p> {{$product->ISBN}}</p>
-                        <p> Athor : {{$product->Author}}</p>
-                        <input type="button" value="Add to cart">
-   
+                            <p> Title : {{$product->product_name}}</p>
+                            <p> Description : {{$product->product_description}}</p>
+                            <p> Price : {{$product->product_price}}</p>
+                            <p> {{$product->ISBN}}</p>
+                            <p> Athor : {{$product->Author}}</p>
+                            <input type="button" value="Add to cart">
                         </div> 
-    
-                        </div>
-                        @endforeach 
-                    </div>
-                </div>
+                     </div>
+                @endforeach 
             </div>
         </div>
 </body>
