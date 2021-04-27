503b8562ef86bcd10099d5d7649fb68c6ba161b8
Wael Maseha <it@waelmaseha.com>
2021-04-27 10:12:28 -0400

    La fin de la premère essaie de créer les classes métiers

--- database/migrations/2021_04_27_021640_create_command_line_table.php
+++ database/migrations/2021_04_27_021640_create_command_line_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
+class CreateCommandLineTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateCartsTable extends Migration
      */
     public function up()
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('command_line', function (Blueprint $table) {
             $table->id();
-            
-            $table->enum('status', ['init', 'progress', 'paid']);
-            $table->double('total');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateCartsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('command_line');
     }
 }
