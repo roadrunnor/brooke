e488cf797c88b84bbc1b7e7a6af604119098e8a0
Wael Maseha <it@waelmaseha.com>
2021-05-06 13:19:39 -0400

    Iinitialisation Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,24 @@
 <?php
-use App\Http\Controllers\clientController;
-use App\Http\Controllers\DetailsController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CartController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('user',[UserController::class,'index'])->name('user.index');
-Route::get('/posts',[clientController::class,'getAllPost'])->name('posts.getallposts');
-Route::get('/posts/{id}',[clientController::class,'getPostById'])->name('posts.getPostById');
-Route::get('/add-post',[clientController::class,'addPost'])->name('posts.addPost');
-Route::get('/update-post', [clientController::class,'updatePost'])->name('posts-update');
-Route::get('/delete-post/{id}',[clientController::class,'deletePost'])->name('post.delete');
-Route::get('/login',[LoginController::class,'index'])->name('login.index');
-Route::post('/login',[LoginController::class,'loginSubmit'])->name('login.submit');
-Route::get('/',[ProductController::class, 'getProducts'])->name('getAll.getProducts');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//---------Routage panier--------------------
-Route::get('/cart',[CartController::class, 'index'])->name('cart.show');
-Route::get('/addProduct/{product_id}/{user_id}',[CartController::class, 'addProduct'])->name('cart.addProduct');
-Route::get('/destroyCommandLine/{id}',[CartController::class, 'destroy'])->name('destroy.show');
-Route::get('/destroyCart/{id}',[CartController::class, 'destroyCart'])->name('destroyCart.cart');
-
-//-----------Fin de routage panier--------------
-
-Route::get('/details/{product_id}', [ProductController::class, 'getDetail']);
-
+require __DIR__.'/auth.php';
