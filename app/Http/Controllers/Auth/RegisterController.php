fc0d7c295731dadde3257c7c6637957fe730b5e4
Wael Maseha <it@waelmaseha.com>
2021-05-13 20:39:43 -0400

    Après supprimer quelques fichiers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use CreateCartsTable;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -74,4 +75,10 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    //--------------------------------
+    // function public CreateCartsTable(){
+    //     return $cart
+    // }
+    //----------------------------------
 }
